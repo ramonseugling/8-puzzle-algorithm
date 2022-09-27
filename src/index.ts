1b21ab6fde17c0b8c457915ebf898698d59498b9
Denys Gaspar <53532689+dgaspar1@users.noreply.github.com>
2022-09-27 18:26:02 -0300

    main refactor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Board } from './entities/board'
 import { ClosedList } from './entities/closed-list'
 import { Coordinate } from './entities/coordinate'
+import { Neighbor } from './entities/neighbor'
 import { Node } from './entities/node'
 import { OpenList } from './entities/open-list'
 import { ProgramParams } from './entities/program-params'
@@ -11,6 +12,31 @@ function getProgramParams() : ProgramParams {
         targetCoordinate: new Coordinate(0, 2),
         boardMatrix: { width: 3, heigh: 3 }
     }
+}
+
+function getValidNeighbors(board: Board, node: Node) : Neighbor[] {
+    let neighbors = node.getNeighbors()
+    neighbors = neighbors.filter((item) => {
+        return board.hasNodeByCoordinate(item.node.getCoordinate())
+    })
+    return neighbors
+}
+
+function calculateNeighborsTargetDistance(neighbors: Neighbor[], target: Node): void {
+    for (let index = 0; index < neighbors.length; index++) {
+        neighbors[index].node.setTarget(target.getCoordinate())
+    }
+}
+
+function addNeighborsToOpenList(neighbors: Neighbor[], openList: OpenList): void {
+    for (let index = 0; index < neighbors.length; index++) {
+        openList.add(neighbors[index].node)
+    }
+}
+
+function problemSolved(current: Node, target: Node): boolean {
+    return current.getCoordinate().getXAxis() === target.getCoordinate().getXAxis() &&
+    current.getCoordinate().getYAxis() === target.getCoordinate().getYAxis()
 }
 
 function main() {
@@ -26,19 +52,13 @@ function main() {
     do {
         current = openList.getFirst()
         board.show(current.getCoordinate(), target.getCoordinate())
-        let neighbors = current.getNeighbors()
-        neighbors = neighbors.filter((item) => {
-            return board.hasNodeByCoordinate(item.node.getCoordinate())
-        })
-
-        for (let index = 0; index < neighbors.length; index++) {
-            neighbors[index].node.setTarget(target.getCoordinate())
-            openList.add(neighbors[index].node)
-        }
+        const neighbors = getValidNeighbors(board, current)
+        calculateNeighborsTargetDistance(neighbors, target)
+        addNeighborsToOpenList(neighbors, openList)
         closedList.add(openList.exitFirst())
         openList.sort()
-    } while (!(current.getCoordinate().getXAxis() === target.getCoordinate().getXAxis() &&
-       current.getCoordinate().getYAxis() === target.getCoordinate().getYAxis()))
+    } while (!problemSolved(current, target))
+
     console.log('Atingimos o objetivo')
     console.log('Quantidade de iterações: ', closedList.getNodes().length - 1)
     console.log('Quantidad de nodos visitados: ', closedList.getNodes().length )
